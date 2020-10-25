00d611f808df75c37d04cd8b4b8bdf1712acb97a
Nikolay Kolyshev <sword.of.destiny2001@mail.ru>
2020-10-25 11:30:19 +0300

    Updates

---
+++ precache-manifest.3c33a9de642b7adb07a366804d27d0a0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff64898ba24f033c84704b9330c93bf0",
+    "url": "/Vikings/index.html"
+  },
+  {
+    "revision": "f6b8b006dd1c21eaf08a",
+    "url": "/Vikings/static/css/main.31f99c7f.chunk.css"
+  },
+  {
+    "revision": "3533e26b1764727691c1",
+    "url": "/Vikings/static/js/2.1029c271.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Vikings/static/js/2.1029c271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b8b006dd1c21eaf08a",
+    "url": "/Vikings/static/js/main.b7cb17a7.chunk.js"
+  },
+  {
+    "revision": "c42f6af1c6b1f125622c",
+    "url": "/Vikings/static/js/runtime-main.754d8c39.js"
+  },
+  {
+    "revision": "cdf92e07070105f1538138cc8a9bf451",
+    "url": "/Vikings/static/media/1.cdf92e07.svg"
+  },
+  {
+    "revision": "4d92cf091d14ff3e239e3bd3641989bf",
+    "url": "/Vikings/static/media/2.4d92cf09.svg"
+  },
+  {
+    "revision": "45014c63160aef5237e8ee51bae321ac",
+    "url": "/Vikings/static/media/3.45014c63.svg"
+  },
+  {
+    "revision": "44852044f623d8d03f1ec97d2b991652",
+    "url": "/Vikings/static/media/4.44852044.svg"
+  },
+  {
+    "revision": "9d97bd4c2598240c263635da7322a257",
+    "url": "/Vikings/static/media/contact-back.9d97bd4c.jpg"
+  },
+  {
+    "revision": "759629a34c67fd390a3339c146da3e10",
+    "url": "/Vikings/static/media/logo.759629a3.svg"
+  },
+  {
+    "revision": "97909454223d2ebf472d0a00a5f75d68",
+    "url": "/Vikings/static/media/main-back.97909454.jpg"
+  }
+]);
